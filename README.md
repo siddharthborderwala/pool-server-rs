15d4c44fe780a1dc505812fa1d8792714654e938
Siddharth Borderwala <siddharthborderwala@gmail.com>
2021-10-13 11:40:12 +0530

    multi-threaded server

---
+++ README.md
@@ -0,0 +1,3 @@
+# rust-server
+
+A multi-threaded web server using `net` module from `rust::std`, implemented with a thread-pool and channels.
---
+++ src/bin/main.rs
@@ -0,0 +1,49 @@
+use std::fs;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
+use server::ThreadPool;
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer: [u8; 1024] = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "views/hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "views/404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
+fn main() {
+    // get the port
+    let port = std::env::args()
+        .nth(1)
+        .or(Some("7878".to_string()))
+        .unwrap();
+    // setup the listener
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).unwrap();
+    // announce listening
+    println!("🚀 Server listening at http://127.0.0.1:{}", port);
+
+    let pool = ThreadPool::new(100);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        pool.execute(move || handle_connection(stream));
+    }
+}
